3b5d74822267b9ebb6f3a6e3251f36f2b5640e93
Richard Willis <willis.rh@gmail.com>
2014-07-24 15:16:33 +0100

    [ORB-239] Implemented timepicker widget, removed "second surgery start" and "second surgery stop" fields.

--- views/default/form_Element_OphNuIntraoperative_TimeTracking.php
+++ views/default/form_Element_OphNuIntraoperative_TimeTracking.php
@@ -17,117 +17,40 @@
  * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
  */
 ?>
-	<div class="element-fields">
-		<div id="div_Element_OphNuIntraoperative_TimeTracking_enters_or" class="row field-row">
-			<div class="large-3 column">
-				<label for="Element_OphNuIntraoperative_TimeTracking_enters_or">
-					<?php echo $element->getAttributeLabel('enters_or')?>:
-				</label>
-			</div>
-			<div class="large-9 column">
-				<div class="row field-row">
-					<div class="large-1 column">
-						<?php echo $form->textField($element, 'enters_or', array('nowrapper' => true))?>
-					</div>
-					<div class="large-2 column">
-						<button type="submit" class="secondary small time-now" data-target="enters_or">Now</button>
-					</div>
-					<div class="large-3 column">
-						<label for="Element_OphNuIntraoperative_TimeTracking_surgery_stop">
-							<?php echo $element->getAttributeLabel('surgery_stop')?>:
-						</label>
-					</div>
-					<div class="large-1 column">
-						<?php echo $form->textField($element, 'surgery_stop', array('nowrapper' => true), array(), array('label' => 3, 'field' => 1))?>
-					</div>
-					<div class="large-2 column end">
-						<button type="submit" class="secondary small time-now" data-target="surgery_stop">Now</button>
-					</div>
-				</div>
-			</div>
+<?php
+$htmlOptions = array();
+$layoutColumns = array(
+	'label' => 6,
+	'field' => 6
+);
+?>
+<div class="element-fields">
+
+	<div class="row field-row">
+		<div class="large-6 column">
+			<?php echo $form->timePicker($element, 'enters_or', $htmlOptions, $layoutColumns);?>
 		</div>
-		<div id="div_Element_OphNuIntraoperative_TimeTracking_patient_time_out" class="row field-row">
-			<div class="large-3 column">
-				<label for="Element_OphNuIntraoperative_TimeTracking_patient_time_out">
-					<?php echo $element->getAttributeLabel('time_out')?>:
-				</label>
-			</div>
-			<div class="large-9 column">
-				<div class="row field-row">
-					<div class="large-1 column">
-						<?php echo $form->textField($element, 'time_out', array('nowrapper' => true), array(), array('label' => 3, 'field' => 1))?>
-					</div>
-					<div class="large-2 column">
-						<button type="submit" class="secondary small time-now" data-target="time_out">Now</button>
-					</div>
-					<div class="large-3 column">
-						<label for="Element_OphNuIntraoperative_TimeTracking_second_surgery_stop">
-							<?php echo $element->getAttributeLabel('second_surgery_stop')?>:
-						</label>
-					</div>
-					<div class="large-1 column">
-						<?php echo $form->textField($element, 'second_surgery_stop', array('nowrapper' => true), array(), array('label' => 3, 'field' => 1))?>
-					</div>
-					<div class="large-2 column end">
-						<button type="submit" class="secondary small time-now" data-target="second_surgery_stop">Now</button>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div id="div_Element_OphNuIntraoperative_TimeTracking_surgery_start" class="row field-row">
-			<div class="large-3 column">
-				<label for="Element_OphNuIntraoperative_TimeTracking_surgery_start">
-					<?php echo $element->getAttributeLabel('surgery_start')?>:
-				</label>
-			</div>
-			<div class="large-9 column">
-				<div class="row field-row">
-					<div class="large-1 column">
-						<?php echo $form->textField($element, 'surgery_start', array('nowrapper' => true), array(), array('label' => 3, 'field' => 1))?>
-					</div>
-					<div class="large-2 column">
-						<button type="submit" class="secondary small time-now" data-target="surgery_start">Now</button>
-					</div>
-					<div class="large-3 column">
-						<label for="Element_OphNuIntraoperative_TimeTracking_sign_out">
-							<?php echo $element->getAttributeLabel('sign_out')?>:
-						</label>
-					</div>
-					<div class="large-1 column">
-						<?php echo $form->textField($element, 'sign_out', array('nowrapper' => true), array(), array('label' => 3, 'field' => 1))?>
-					</div>
-					<div class="large-2 column end">
-						<button type="submit" class="secondary small time-now" data-target="sign_out">Now</button>
-					</div>
-				</div>
-			</div>
-		</div>
-		<div id="div_Element_OphNuIntraoperative_TimeTracking_second_surgery_start" class="row field-row">
-			<div class="large-3 column">
-				<label for="Element_OphNuIntraoperative_TimeTracking_second_surgery_start">
-					<?php echo $element->getAttributeLabel('second_surgery_start')?>:
-				</label>
-			</div>
-			<div class="large-9 column">
-				<div class="row field-row">
-					<div class="large-1 column">
-						<?php echo $form->textField($element, 'second_surgery_start', array('nowrapper' => true), array(), array('label' => 3, 'field' => 1))?>
-					</div>
-					<div class="large-2 column">
-						<button type="submit" class="secondary small time-now" data-target="second_surgery_start">Now</button>
-					</div>
-					<div class="large-3 column">
-						<label for="Element_OphNuIntraoperative_TimeTracking_leaves_or">
-							<?php echo $element->getAttributeLabel('leaves_or')?>:
-						</label>
-					</div>
-					<div class="large-1 column end">
-						<?php echo $form->textField($element, 'leaves_or', array('nowrapper' => true), array(), array('label' => 3, 'field' => 1))?>
-					</div>
-					<div class="large-2 column end">
-						<button type="submit" class="secondary small time-now" data-target="leaves_or">Now</button>
-					</div>
-				</div>
-			</div>
+		<div class="large-6 column">
+			<?php echo $form->timePicker($element, 'surgery_stop', $htmlOptions, $layoutColumns);?>
 		</div>
 	</div>
+
+	<div class="row field-row">
+		<div class="large-6 column">
+			<?php echo $form->timePicker($element, 'time_out', $htmlOptions, $layoutColumns);?>
+		</div>
+		<div class="large-6 column">
+			<?php echo $form->timePicker($element, 'surgery_start', $htmlOptions, $layoutColumns);?>
+		</div>
+	</div>
+
+	<div class="row field-row">
+		<div class="large-6 column">
+			<?php echo $form->timePicker($element, 'sign_out', $htmlOptions, $layoutColumns);?>
+		</div>
+		<div class="large-6 column">
+			<?php echo $form->timePicker($element, 'leaves_or', $htmlOptions, $layoutColumns);?>
+		</div>
+	</div>
+
+</div>
